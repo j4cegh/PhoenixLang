138ebdb7c31fb00d5d3bc2d407e8175ceba23170
j4ck <50369519+j4cegh@users.noreply.github.com>
2022-04-23 02:17:20 +0300

    misc: `MethodName` allow only get

--- PhoenixLang/Core/MethodAttribute.cs
+++ PhoenixLang/Core/MethodAttribute.cs
@@ -3,7 +3,7 @@ namespace PhoenixLang.Core;
 [AttributeUsage(AttributeTargets.Method)]
 public class MethodAttribute : Attribute
 {
-    public string MethodName = "";
+    public string MethodName { get; }
     
     public MethodAttribute(string methodName)
     {
