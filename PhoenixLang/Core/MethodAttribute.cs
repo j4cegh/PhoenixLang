45b0963cd0a9c26aebec7fac7d72ad3a33b065cb
j4ck <cacodemon1123@gmail.com>
2022-04-23 16:54:27 +0300

    misc: remove useless assigning

--- PhoenixLang/Core/MethodAttribute.cs
+++ PhoenixLang/Core/MethodAttribute.cs
@@ -3,10 +3,8 @@ namespace PhoenixLang.Core;
 [AttributeUsage(AttributeTargets.Method)]
 public class MethodAttribute : Attribute
 {
-    public string MethodName { get; }
-    
     public MethodAttribute(string methodName)
     {
-        MethodName = methodName;
+        
     }
 }
--- PhoenixLang/Core/StatementAttribute.cs
+++ PhoenixLang/Core/StatementAttribute.cs
@@ -3,10 +3,8 @@ namespace PhoenixLang.Core;
 [AttributeUsage(AttributeTargets.Method)]
 public class StatementAttribute : Attribute
 {
-    public string StatementName { get; }
-    
     public StatementAttribute(string statementName)
     {
-        StatementName = statementName;
+        
     }
 }
